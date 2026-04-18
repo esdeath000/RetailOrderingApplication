3c9c028305a96876af950dd300d1c650a7809639
ken314526 <abhisheksharma801831@gmail.com>
2026-04-18 13:30:14 +0530

    Initial commit

---
+++ RetailOrderingApplication/src/main/java/com/hcl/retailorderingapplication/RetailOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.retailorderingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailOrderingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailOrderingApplication.class, args);
+    }
+
+}
---
+++ RetailOrderingApplication/src/test/java/com/hcl/retailorderingapplication/RetailOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.retailorderingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailOrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
